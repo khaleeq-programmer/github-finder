0a384dc22425d5c8aa89e508facd42b77e10c130
khaleeq-programmer <khaleeq.programmer@gmail.com>
2020-09-24 20:53:21 +0500

    first component Navbar created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {Component}from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        hello from react
-      </h1>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className='App'>
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
